f1b2e770e3ad24c2828c95b43d0170b233c9ec5e
Bruce Ren <renliangfeng2011@gmail.com>
2024-07-04 14:38:43 +1000

    fixed issue for windows batch script

--- setup-iiq-mssql.bat
+++ setup-iiq-mssql.bat
@@ -13,24 +13,32 @@ docker cp iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/database/create_i
 set /p continue=By default IIQ database name is 'identityiq'. But you can modify %cd%/create_identityiq_tables.sqlserver to override values before pressing Enter to continue.
 
 @REM upload IIQ DB script to IIQ DB container
-docker cp ./create_identityiq_tables.sqlserver iq-mssql-db:/tmp/
+docker cp ./create_identityiq_tables.sqlserver iiq-mssql-db:/tmp/
 
 @REM upload shell script to IIQ DB container
 docker cp ./shell/create-iiq-db-mssql.sh iiq-mssql-db:/tmp/
 docker exec --workdir /tmp iiq-mssql-db chmod 755 create-iiq-db-mssql.sh
 
 @REM run shell script in IIQ DB container to create IIQ DB & tables
-docker exec -it iiq-mssql-db -c "/tmp/reate-iiq-db-mssql.sh $rootPassword"
+docker exec -it iiq-mssql-db sh -c "/tmp/create-iiq-db-mssql.sh %rootPassword%"
 
 @REM download sp.init-custom.xml from IIQ App container
 docker cp iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml .
 
-set /p overrideInitCustom=You can modify %cd%/sp.init-custom.xml to customise the files you want to import to IIQ. Type 'y' or 'Y' to override; Leave it empty or type any other values to skip. Press Enter to continue. "
+set "overrideInitCustom=n"
+set /p overrideInitCustom=You can modify %cd%/sp.init-custom.xml to customise the files you want to import to IIQ. Type 'y' or 'Y' to override; Leave it empty or type any other values to skip. Press Enter to continue.
 
-if %overrideInitCustom% == y OR %overrideInitCustom% == Y (
-	echo upload %cd%/sp.init-custom.xml to override
-	docker cp ./sp.init-custom.xml iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml
+if not %overrideInitCustom% == y (
+	if not %overrideInitCustom% == Y (
+		echo not to override sp.init-custom.xml
+		GOTO importXML
+	)
 )
+
+echo upload %cd%/sp.init-custom.xml to override
+docker cp ./sp.init-custom.xml iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml
+
+:importXML
 
 @REM overried iiq (shell script to run iiq console) to increase JVM HEAP
 docker cp ./shell/iiq iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/bin/iiq
--- setup-iiq.bat
+++ setup-iiq.bat
@@ -20,17 +20,25 @@ docker cp ./shell/create-iiq-db.sh iiq-db:/tmp/
 docker exec --workdir /tmp iiq-db chmod 755 create-iiq-db.sh
 
 @REM run shell script in IIQ DB container to create IIQ DB & tables
-docker exec -it iiq-db sh -c "/tmp/create-iiq-db.sh $rootPassword"
+docker exec -it iiq-db sh -c "/tmp/create-iiq-db.sh %rootPassword%"
 
 @REM download sp.init-custom.xml from IIQ App container
 docker cp iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml .
 
-set /p overrideInitCustom=You can modify %cd%/sp.init-custom.xml to customise the files you want to import to IIQ. Type 'y' or 'Y' to override; Leave it empty or type any other values to skip. Press Enter to continue. "
+set "overrideInitCustom=n"
+set /p overrideInitCustom=You can modify %cd%/sp.init-custom.xml to customise the files you want to import to IIQ. Type 'y' or 'Y' to override; Leave it empty or type any other values to skip. Press Enter to continue.
 
-if %overrideInitCustom% == y OR %overrideInitCustom% == Y (
-	echo upload %cd%/sp.init-custom.xml to override
-	docker cp ./sp.init-custom.xml iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml
+if not %overrideInitCustom% == y (
+	if not %overrideInitCustom% == Y (
+		echo not to override sp.init-custom.xml
+		GOTO importXML
+	)
 )
+
+echo upload %cd%/sp.init-custom.xml to override
+docker cp ./sp.init-custom.xml iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/config/sp.init-custom.xml
+
+:importXML
 
 @REM overried iiq (shell script to run iiq console) to increase JVM HEAP
 docker cp ./shell/iiq iiq-app:/usr/local/tomcat/webapps/identityiq/WEB-INF/bin/iiq

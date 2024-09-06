1c841a3f6b07d1dd3a3240cbdb448aa8534de520
Bruce Ren <renliangfeng2011@gmail.com>
2024-09-06 13:38:40 +1000

    Update init-iiq.sh
    Import LCM and RapidSetup objects

--- shell/init-iiq.sh
+++ shell/init-iiq.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 cd /usr/local/tomcat/webapps/identityiq/WEB-INF/bin
 
+echo "Start to import init.xml"
 echo "import init.xml" >> import.txt
-
 ./iiq console < import.txt
-
 rm import.txt
+echo "Complete import init.xml"
+
+if [ -e ../config/init-lcm.xml ]
+then
+	echo "Start to import init-lcm.xml"
+	echo "import init-lcm.xml" >> import-lcm.txt
+	./iiq console < import-lcm.txt
+	rm import-lcm.txt
+	echo "Complete import init-lcm.xml"
+fi
+
+if [ -e ../config/init-rapidsetup.xml ]
+then
+    echo "Start to import init-rapidsetup.xml"
+	echo "import init-rapidsetup.xml" >> import-rapidsetup.txt
+	./iiq console < import-rapidsetup.txt
+	rm import-rapidsetup.txt
+	echo "Complete import init-rapidsetup.xml"
+fi
